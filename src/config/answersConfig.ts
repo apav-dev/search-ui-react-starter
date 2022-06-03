92cfc53d0a9812e0a98dc9d05ddde1b91baaf08e
Aaron Pavlick <apavlick@yext.com>
2022-06-03 17:37:33 -0400

    initial commit

---
+++ src/config/answersConfig.ts
@@ -0,0 +1,20 @@
+export const answersConfig = {
+  apiKey: "39ad744df1bc69e3c7fe27a666eae266",
+  experienceKey: "toast-v2",
+  locale: "en",
+  verticalKey: "beverages",
+  endpoints: {
+    universalSearch:
+      "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/query",
+    verticalSearch:
+      "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/vertical/query",
+    questionSubmission:
+      "https://liveapi-sandbox.yext.com/v2/accounts/me/createQuestion",
+    universalAutocomplete:
+      "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/autocomplete",
+    verticalAutocomplete:
+      "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/vertical/autocomplete",
+    filterSearch:
+      "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/filtersearch",
+  },
+};
