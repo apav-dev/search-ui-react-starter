b0a29f645b3268b3d5af6ca7a9071a0c989db93f
Aaron Pavlick <apavlick@yext.com>
2022-07-28 18:29:35 -0400

    search config in provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,12 @@ import { SearchHeadlessProvider } from "@yext/search-headless-react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { searchConfig } from "./config/searchConfig";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <SearchHeadlessProvider
-      apiKey="Your Search API Key"
-      experienceKey="Your Experience Key"
-      verticalKey="Your Vertical Key"
-      locale="en"
-    >
+    <SearchHeadlessProvider {...searchConfig}>
       <App />
     </SearchHeadlessProvider>
   </React.StrictMode>
