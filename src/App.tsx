74d5081a0a760c65effea6a78e74967b54987716
Aaron Pavlick <apavlick@yext.com>
2022-08-01 13:45:30 -0400

    sorting component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {
   VerticalResults,
 } from "@yext/search-ui-react";
 import { useEffect } from "react";
+import SortDropdown from "./components/SortDropdown";
 
 function App() {
   const searchActions = useSearchActions();
@@ -17,6 +18,9 @@ function App() {
     <div className="flex justify-center px-4 py-6">
       <div className="w-full max-w-5xl">
         <SearchBar />
+        <div className="flex justify-end mb-4">
+          <SortDropdown />
+        </div>
         <VerticalResults CardComponent={StandardCard} />
       </div>
     </div>
